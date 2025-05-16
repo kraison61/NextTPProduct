b3fb0cd85624ff4bfa47e3d2575601ead12074d7
Oun <oun.cav@gmail.com>
2025-05-16 22:53:42 +0700

    edit contact button

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -62,9 +62,11 @@ const Nav = ({ openNav }: Props) => {
         </div>
         {/* buttons */}
         <div className="flex items-center space-x-4">
+          <Link href="/#footerId">
           <button className="md:px-12 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg">
-            <Link href="/#footerId">ติดต่อเรา</Link>
+            ติดต่อเรา
           </button>
+          </Link>
           {/* Burger Menu */}
           <HiBars3BottomRight
             onClick={openNav}
