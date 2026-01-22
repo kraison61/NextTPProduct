dc6d979694600637421430b16306beab33c92fb6
Oun <oun.cav@gmail.com>
2026-01-22 12:15:36 +0700

    Add Turbopack to next config

--- next.config.ts
+++ next.config.ts
@@ -1,50 +1,53 @@
-import type { NextConfig } from 'next';
-import withBundleAnalyzer from '@next/bundle-analyzer';
+import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const withAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === "true",
 });
 
 const nextConfig: NextConfig = {
+  // ✅ เพิ่มบรรทัดนี้ (สำคัญมาก)
+  turbopack: {},
+
+  // ⚠️ ใช้เฉพาะตอน --webpack
   webpack: (config) => {
     config.watchOptions = {
       ignored: [
-        '**/node_modules/**',
-        '**/C:/Users/COJ/Cookies/**',
-        '**/C:/Users/COJ/Application Data/**',
-        '**/C:/Users/COJ/**',
+        "**/node_modules/**",
+        "**/C:/Users/COJ/Cookies/**",
+        "**/C:/Users/COJ/Application Data/**",
+        "**/C:/Users/COJ/**",
       ],
     };
     return config;
   },
-  
+
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '192.168.220.253',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "192.168.220.253",
+        pathname: "/**",
       },
       {
-        protocol: 'http',
-        hostname: 'kraison.thddns.net',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "kraison.thddns.net",
+        pathname: "/**",
       },
       {
-        protocol: 'http',
-        hostname: '139.59.105.108',
-        port: '9000',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "139.59.105.108",
+        port: "9000",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'minio.speedynetloans.net',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "minio.speedynetloans.net",
+        pathname: "/**",
       },
     ],
     unoptimized: true,
   },
 };
 
-// export แบบใช้ analyzer ห่อ config
 export default withAnalyzer(nextConfig);
